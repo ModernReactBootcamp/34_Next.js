e2652416bb16b41a7121cf1597407ec5e4b31ce9
kdord <katya.koliada49@gmail.com>
2020-01-21 17:41:41 +0200

    add index component

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+const Index = () => (
+  <div>
+    <h1>Our Index Page</h1>
+  </div>
+);
+
+export default Index;
+
+// equivalent
+
+// const Index = () => {
+//     return (
+//         <div>
+//             <h1>Our Index Page</h1>
+//         </div>
+//     )
+// }
