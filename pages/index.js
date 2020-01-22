c360a72c169f64104afd481e0ea880c3b080d02a
kdord <katya.koliada49@gmail.com>
2020-01-22 09:31:21 +0200

    add getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,24 @@
+// import React, { Component } from 'react';
+
+// export default class Index extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     console.log('fetching your data in constructor');
+//     this.state = {};
+//   }
+//   static async getInitialProps() {
+//     return { res: console.log('fetching data inside getInitialProps') };
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>Our Index Page</h1>
+//       </div>
+//     );
+//   }
+// }
+
 const Index = () => {
   return (
     <div>
@@ -6,6 +27,9 @@ const Index = () => {
   );
 };
 export default Index;
+Index.getInitialProps = async () => {
+  return { res: console.log('fetching data inside getInitialProps') };
+};
 
 // equivalent
 
