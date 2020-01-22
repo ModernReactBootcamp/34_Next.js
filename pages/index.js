f394c683f109a7b6f269fe100b0c3aaa8d2605db
kdord <katya.koliada49@gmail.com>
2020-01-22 09:44:43 +0200

    petching posts w/ getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 // import React, { Component } from 'react';
 
 // export default class Index extends Component {
@@ -19,16 +21,24 @@
 //   }
 // }
 
-const Index = () => {
+const Index = ({ posts }) => {
   return (
     <div>
       <h1>Our Index Page</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 };
 export default Index;
-Index.getInitialProps = async () => {
-  return { res: console.log('fetching data inside getInitialProps') };
+Index.getInitialProps = async ctx => {
+  // https://jsonplaceholder.typicode.com/posts
+  const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  const { data } = res;
+  return { posts: data };
 };
 
 // equivalent
