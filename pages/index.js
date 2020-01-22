e9895e3cd1f5fe84be759e277e93b7532617e4d2
kdord <katya.koliada49@gmail.com>
2020-01-22 12:28:51 +0200

    custom server

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ const Index = ({ posts }) => {
       <ul>
         {posts.map(post => (
           <li key={post.id}>
-            <Link href={`/post?id=${post.id}`}>
+            <Link href={`/post?id=${post.id}`} as={`/post/${post.id}`}>
               <a>{post.title}</a>
             </Link>
           </li>
